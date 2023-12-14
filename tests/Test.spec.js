20d55d9d268fe18b9aff3c2dfcd3678d5450e4dc
junaidnasir1001 <45687809+junaidnasir1001@users.noreply.github.com>
2023-12-14 13:07:12 +0500

    first commit

---
+++ tests/Test.spec.js
@@ -0,0 +1,24 @@
+const{test,expect}=require('@playwright/test')
+
+test('This is 1st test on playwright',async ({page})=>{
+
+   await page.goto('http://129.150.40.148:8001/login');
+
+   await page.locator("//input[@name='username']").fill('pukatqa-admin');
+   await page.locator("//input[@name='password']").fill('12345678');
+
+   await page.click("(//button[@type='submit'])[1]")
+
+
+   // await page.getByRole('input',{name:'username'}).fill('pukatqa-admin')
+
+   const pageTitle = page.title();
+
+   await expect(page).toHaveURL("http://129.150.40.148:8001/admin/dashboard");
+
+   console.log( pageTitle)
+
+   await page.close();
+
+
+})
