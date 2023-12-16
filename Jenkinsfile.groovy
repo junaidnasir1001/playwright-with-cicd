f3e3a892e2f8ac0b5e7e81f781009a93955964d9
junaidnasir1001 <45687809+junaidnasir1001@users.noreply.github.com>
2023-12-16 10:45:25 +0500

    update Jenkinsfile in my project

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,52 @@
 pipeline {
     agent any
 
+    environment {
+        // Specify the NodeJS installation defined in Jenkins
+        nodejsInstallation = 'NodeJS'
+    }
+
     stages {
-        stage('Build') {
+        stage('Checkout') {
             steps {
-                echo 'Building..'
+                // Checkout the code from your Git repository
+                checkout scm
             }
         }
-        stage('Test') {
+
+        stage('Install Dependencies') {
             steps {
-                echo 'Testing..'
+                // Use NodeJS installation defined in Jenkins
+                script {
+                    def playwrightInstallation = tool 'NodeJS'
+                    sh "${playwrightInstallation}/bin/npm install"
+                }
             }
         }
+
+        stage('Run Tests') {
+            steps {
+                // Use NodeJS installation defined in Jenkins
+                script {
+                    def playwrightInstallation = tool 'NodeJS'
+                    sh "${playwrightInstallation}/bin/npx playwright test"
+                }
+            }
+        }
+
         stage('Deploy') {
             steps {
-                echo 'Deploying....'
+                echo 'Add deployment steps here if needed...'
+                // Example: Deploy to a hosting platform
+                // sh 'npm run deploy'
             }
+        }
+    }
+
+    post {
+        always {
+            // Clean up any resources, notify, etc.
+            echo 'Pipeline completed.'
         }
     }
 }
